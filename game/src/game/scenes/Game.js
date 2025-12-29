01e0054d40055fa297fe7c858e4767f551adaf0e
unbounddev <unbounddev@git>
2025-12-29 11:48:05 -0800

    [game] Add correct positions for down cards

--- game/src/game/scenes/Game.js
+++ game/src/game/scenes/Game.js
@@ -15,20 +15,48 @@ export class Game extends Scene
       this.state = this.room.state;
       const centerX = this.scale.width/2;
       const centerY = this.scale.height/2;
+      const displayCard = this.add.image(centerX, centerY, 'BACK').setOrigin(0.5);
+
+      /** @typedef {{ x: number, y: number }} Position */
+      /** @typedef {(card: Phaser.GameObjects.Image, i: number) => Position} PositionFunc
+      /** @type {PositionFunc[]} */
+      const DOWN_POSITIONS = [
+        (card, i) => ({ 
+          x: centerX-(card.displayWidth+(10*window.devicePixelRatio)) + (i*(card.displayWidth+(10*window.devicePixelRatio))),
+          y: card.displayHeight+(10*window.devicePixelRatio)
+        }),
+        (card, i) => ({ 
+          x: centerX-(card.displayWidth+(10*window.devicePixelRatio)) + (i*(card.displayWidth+(10*window.devicePixelRatio))),
+          y: this.scale.height-(card.displayHeight+(10*window.devicePixelRatio))
+        }),
+        (card, i) => ({ 
+          x: centerX-(card.displayWidth+(10*window.devicePixelRatio)) + (i*(card.displayWidth+(10*window.devicePixelRatio))),
+          y: this.scale.height-(card.displayHeight+(10*window.devicePixelRatio))
+        }),
+        (card, i) => ({ 
+          x: centerX-(card.displayWidth+(10*window.devicePixelRatio)) + (i*(card.displayWidth+(10*window.devicePixelRatio))),
+          y: this.scale.height-(card.displayHeight+(10*window.devicePixelRatio))
+        }),
+      ]
 
       console.log(this.room.state)
 
-      this.add.image(centerX, centerY, 'BACK').setOrigin(0.5);
       
       // deal down cards
       const downAnimations = [];
+      /** @type {PositionFunc[]} */
+      let downPositions = [];
+      if (this.room.state.players.size == 2){
+        downPositions = [DOWN_POSITIONS[0], DOWN_POSITIONS[2]]
+      }
       for (let i = 0; i < 3; i++){
         for (let j = 0; j < this.room.state.players.size; j++){
           let card = this.add.image(centerX, centerY, 'BACK').setOrigin(0.5);
+          const pos = downPositions[j](card, i);
           downAnimations.push({
             targets: card,
-            x: centerX-(card.displayWidth+(10*window.devicePixelRatio)) + (i*(card.displayWidth+(10*window.devicePixelRatio))),
-            y: this.scale.height-(card.displayHeight+(10*window.devicePixelRatio)),
+            x: pos.x,
+            y: pos.y,
             duration: 200
           })
         }
